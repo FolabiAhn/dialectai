6b8e370f7893db5336f9e0838e024c72b81128ce
Beranger GUEDOU <rodrigueguedou2@gmail.com>
2020-03-13 21:14:09 +0100

    new branch for bahdanau audio

--- attention.py
+++ attention.py
@@ -41,22 +41,22 @@ class BahdanauAttentionBase(nn.Module):
         
 class BahdanauAttentionAudio(nn.Module):
     
-    def __init__(self, kernel_size, kernel_num, units, hidden_size):
+    def __init__(self, kernel_size, units, hidden_size):
         super().__init__()
         self.prev_att = torch.zeros(20, 198, 1).to('cuda')#None
         self.W1 = nn.Linear(hidden_size, units)
         self.W2 = nn.Linear(hidden_size, units)
         self.V = nn.Linear(units, 1)
-        self.loc_conv = nn.Conv1d(198, kernel_num, kernel_size=2*kernel_size+1,
+        self.loc_conv = nn.Conv1d(in_channels=198, out_channels=198, kernel_size=2*kernel_size+1,
                                   padding=kernel_size, bias=False)
         self.loc_proj = nn.Linear(1, hidden_size, bias=False)
  
-    def reset_mem(self):
-        super().reset_mem()
-        self.prev_att = None
+    #def reset_mem(self):
+    #    super().reset_mem()
+    #    self.prev_att = None
 
-    def set_mem(self, prev_att):
-        self.prev_att = prev_att
+    #def set_mem(self, prev_att):
+    #    self.prev_att = prev_att
         
     def forward(self, query, values):
         # hidden shape == (batch_size, hidden size)
@@ -66,13 +66,17 @@ class BahdanauAttentionAudio(nn.Module):
         hidden_with_time_axis = torch.unsqueeze(query, 1)
 
         # Calculate location context
-        #convolve = self.loc_conv(self.prev_att)
-        #loc_context = self.loc_proj(convolve)
+        convo = self.loc_conv(self.prev_att)
+        loc_context = self.loc_proj(convo)
+        
+        #print("loc_context", loc_context.shape)
         
         # score shape == (batch_size, max_length, 1)
         # we get 1 at the last axis because we are applying score to self.V
         # the shape of the tensor before applying self.V is (batch_size, max_length, units)
-        sum_1 = self.W1(values) + self.W2(hidden_with_time_axis) #+ loc_context
+        sum_1 = self.W1(values) + self.W2(hidden_with_time_axis) + loc_context
+        
+        #print("sum_1", sum_1.shape)
         score = self.V(torch.tanh(sum_1))
         # As we are dealing with audio we will take the topk frames
         top_val, top_pos = torch.topk(score, k=100, dim=1)
@@ -80,8 +84,7 @@ class BahdanauAttentionAudio(nn.Module):
         top_pos = top_pos.squeeze(2)
         score = -1 * (score.scatter(1, top_pos, 0) - score)
         score = score.unsqueeze(2)
-        # We will change the softmax with the smothing
-        
+        # We will change the softmax with the smoothing
         attention_weights = smoothing(score)
         self.prev_att = attention_weights
         
@@ -163,12 +166,12 @@ class SuperHeadAttention(nn.Module):
         self.W = nn.Linear(self.nbr_heads, 1)
         self.attention_1 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
         self.attention_2 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
-        self.attention_3 = LuongAttentionDot()
-        self.attention_4 = LuongAttentionDot()
-        self.attention_5 = LuongAttentionConcat(units=units, hidden_size=hidden_size)
-        self.attention_6 = LuongAttentionConcat(units=units, hidden_size=hidden_size)
-        self.attention_7 = LuongAttentionGeneral(hidden_size=hidden_size)
-        self.attention_8 = LuongAttentionGeneral(hidden_size=hidden_size)
+        self.attention_3 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
+        self.attention_4 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
+        self.attention_5 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
+        self.attention_6 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
+        self.attention_7 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
+        self.attention_8 = BahdanauAttentionBase(units=units, hidden_size=hidden_size)
         
     def forward(self, query, values):
         
@@ -185,8 +188,17 @@ class SuperHeadAttention(nn.Module):
                             score_5, score_6, score_7, score_8], dim=2)
         #print('tata',concat.shape)
         score = self.W(concat)
+        # As we are dealing with audio we will take the topk frames
+        top_val, top_pos = torch.topk(score, k=100, dim=1)
+        score = score.squeeze(2)
+        top_pos = top_pos.squeeze(2)
+        score = -1 * (score.scatter(1, top_pos, 0) - score)
+        score = score.unsqueeze(2)
+        # We will change the softmax with the smothing
+        
+        attention_weights = smoothing(score)
         #print('toto', score.shape)
-        attention_weights = F.softmax(score, dim=1)
+        #attention_weights = F.softmax(score, dim=1)
         # context_vector shape after sum == (batch_size, hidden_size) 
         # (values == EO)
         context_vector = attention_weights * values
